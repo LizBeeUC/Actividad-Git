25541e2c130ae3cf4e64e30735aa0abe47b8e6b7
Alejandro Tapia <130942603+LizBeeUC@users.noreply.github.com>
2026-05-14 11:29:27 -0400

    Codigo añadido

---
+++ codigo.py
@@ -0,0 +1,12 @@
+import random
+
+dado = random.randint(1, 20)
+bonus_fuerza = 2
+tirada_de_fuerza = dado + bonus_fuerza
+
+print(tirada_de_fuerza)
+
+if tirada_de_fuerza > 15:
+    print("Éxito en la tirada de fuerza")
+else:
+    print("Fallo en la tirada de fuerza")
